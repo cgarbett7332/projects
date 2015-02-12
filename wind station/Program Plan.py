d5035e75228db171f0f741165703827af08b3478
cgarbett7332 <cgarbett7332@sohamcollege.org.uk>
2015-02-12 14:43:23 +0000

    none

--- wind station/Program Plan.py
+++ wind station/Program Plan.py
@@ -48,14 +48,12 @@ while True:
 ############################################
 
 def StoreRainCount():
-    raincount = bucket_tipped(channel)
-    raincount.append(input)
+    raincount = bucket_tipped
     with open ("raincount.txt",mode='w') as file:
         json.dump(raincount,file)
 
 def StoreWindCount():
-    windcount = spin(channel)
-    windcount.append(input)
+    windcount = spin()
     with open ("windcount.txt",mode='w') as file:
         json.dump(windcount,file)
 
